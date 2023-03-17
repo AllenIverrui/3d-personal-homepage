87fe063073693874970b89bc0c4fdf9eab72cfab
Allen Iverrui <3037256239@qq.com>
2023-03-17 22:16:15 +0800

    first commit

---
+++ src/resources/textures.js
@@ -0,0 +1,63 @@
+let billboardTextures = {};
+// 第一块展板图片地址
+billboardTextures.blogTexture = '../src/jsm/blog.png';
+// 第二块展板图片地址
+billboardTextures.musicTexture =
+  '../src/jsm/music.png';
+// 第三块展板图片地址
+billboardTextures.fundTexture =
+  '../src/jsm/fund.png';
+
+let boxTexture = {};
+// 链接的图标地址
+boxTexture.Github = '../src/jsm/githubLogo.png';
+boxTexture.BiliBili = '../src/jsm/BiliBili.png';
+boxTexture.QQ = '../src/jsm/qq.png';
+boxTexture.mail = '../src/jsm/envelope.png';
+boxTexture.reactIcon = '../src/jsm/react.png';
+boxTexture.allSkills = '../src/jsm/allSkills.png';
+boxTexture.lensFlareMain = '../src/jsm/lensflare0.png';
+boxTexture.skrillex = '../src/jsm/lzw2.jpg';
+boxTexture.edmText = '../src/jsm/EDM.png';
+
+// 砖块材质
+let stoneTexture = '../src/jsm/stone.png';
+// 展板木头材质
+let woodTexture = '../src/jsm/woodTexture.jpg';
+
+// 文字图片地址
+let inputText = {};
+inputText.terpSolutionsText = '../src/jsm/terp-solutions-text.png';
+inputText.activities = '../src/jsm/activities_text.jpg';
+inputText.bagholderBetsText = '../src/jsm/bagholderbets-text.png';
+inputText.homeSweetHomeText = '../src/jsm/home-sweet-home-text.png';
+inputText.staticPortfolio = '../src/jsm/static-portfolio.png';
+inputText.pcControl = '../src/jsm/pc-control.png'
+inputText.mobileControl = '../src/jsm/mobile-control.png'
+inputText.link = '../src/jsm/link.png'
+
+//SVG
+let SVG = {};
+SVG.reactLogo = '../src/jsm/react-svg.svg';
+
+// 链接的跳转地址
+let URL = {};
+URL.blog =
+  'http://blog.alleniverrui.top';
+URL.ryanfloyd = 'http://blog.alleniverrui.top';
+URL.fund = 'http://alleniverrui.top';
+URL.gitHub = 'https://github.com/AllenIverrui';
+URL.BiliBili = 'https://space.bilibili.com/433569583';
+URL.email = 'mailto:3037256239@qq.com';
+URL.music = 'http://jsp-music.alleniverrui.top';
+URL.devTo =
+  'https://wpa.qq.com/msgrd?v=3&uin=3037256239&site=qq&menu=yes&jumpflag=1';
+
+export {
+  billboardTextures,
+  boxTexture,
+  inputText,
+  URL,
+  stoneTexture,
+  woodTexture,
+};
